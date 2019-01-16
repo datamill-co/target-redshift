ffd444e59a4485dcdcd1f936aa24b245124f632f
AlexanderMann <mannimal09@gmail.com>
2019-01-16 15:06:19 -0600

    Fix: Redshift doesn't support empty column table creation

--- target_redshift/redshift.py
+++ target_redshift/redshift.py
@@ -1,3 +1,4 @@
+from copy import deepcopy
 import re
 
 from psycopg2 import sql
@@ -17,12 +18,25 @@ class RedshiftError(PostgresError):
     """
 
 
+def _make_schema_nullable(schema):
+    # Redshift does not allow for creation of columns which are non null without a default
+    nullable_schema = deepcopy(schema)
+    nullable_properties = nullable_schema['properties']
+
+    for field in nullable_properties:
+        nullable_properties[field] = json_schema.make_nullable(nullable_properties[field])
+
+    return nullable_schema
+
+
 class RedshiftTarget(PostgresTarget):
     """
     Placeholder for specific Redshift implementation of a Singer Target.
     """
 
     MAX_VARCHAR = 65535
+    CREATE_TABLE_INITIAL_COLUMN = '_sdc_target_redshift_create_table_placeholder'
+    CREATE_TABLE_INITIAL_COLUMN_TYPE = 'BOOLEAN'
 
     def __init__(self, connection, *args, redshift_schema='public', **kwargs):
         self.LOGGER.info(
@@ -31,6 +45,27 @@ class RedshiftTarget(PostgresTarget):
 
         self.conn = connection
         self.postgres_schema = redshift_schema
+
+    def add_table(self, cur, name, metadata):
+        self._validate_identifier(name)
+
+        create_table_sql = sql.SQL('CREATE TABLE {}.{} ({} {})').format(
+            sql.Identifier(self.postgres_schema),
+            sql.Identifier(name),
+            # Redshift does not allow for creation of tables with no columns
+            sql.Identifier(self.CREATE_TABLE_INITIAL_COLUMN),
+            sql.SQL(self.CREATE_TABLE_INITIAL_COLUMN_TYPE))
+
+        cur.execute(sql.SQL('{};').format(
+            create_table_sql))
+
+        self._set_table_metadata(cur, name, {'version': metadata.get('version', None)})
+
+        self.add_column_mapping(cur,
+                                name,
+                                (self.CREATE_TABLE_INITIAL_COLUMN,),
+                                self.CREATE_TABLE_INITIAL_COLUMN,
+                                json_schema.make_nullable({'type': json_schema.BOOLEAN}))
 
     def sql_type_to_json_schema(self, sql_type, is_nullable):
         if sql_type == 'character varying':
