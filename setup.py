d22a4acf669ac35a5ef3112384ab1a64365c7826
AlexanderMann <mannimal09@gmail.com>
2019-01-28 13:31:01 -0600

    Housekeeping: PyPi metadata, description

--- setup.py
+++ setup.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python
 
+from os import path
+
 from setuptools import setup, find_packages
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='target-redshift',
+    url='https://github.com/datamill-co/target-redshift',
+    author='datamill',
     version="0.0.0",
     description='Singer.io target for loading data into redshift',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     classifiers=['Programming Language :: Python :: 3 :: Only'],
     py_modules=['target_redshift'],
     install_requires=[
