75e57c8f6b990528fa9310fceb4a599996a7877b
AlexanderMann <mannimal09@gmail.com>
2019-03-15 11:44:35 -0500

    Housekeeping: Bump Singer Target Postgres to 0.1.6

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     py_modules=['target_redshift'],
     install_requires=[
         'boto3==1.9.79',
-        'singer-target-postgres==0.1.5',
+        'singer-target-postgres==0.1.6',
         'urllib3==1.24.1'
     ],
     setup_requires=[
