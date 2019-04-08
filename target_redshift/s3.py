147c77bd34ed6c96cf4d512ce0c07fa56998b66e
Mirela Andreea Grigoras <magrigoras@bitdefender.com>
2019-04-08 21:36:28 +0300

    Handle hex characters.

--- target_redshift/s3.py
+++ target_redshift/s3.py
@@ -47,5 +47,5 @@ class _EncodeBinaryReadable:
             line = self.input.read()
             if line == '':
                 return output
-            output += line.encode('utf-8')
+            output +=  re.sub(r'[^\x00-\x7f]', r'', line).encode('utf-8')
         return output
